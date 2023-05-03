9285f982f7f89efc4ed3d4ce535dd5c63c907bbc
Michael <michael.c.hadley@gmail.com>
2023-05-03 15:33:49 -0400

    Basic start page creation

---
+++ src/components/common/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Michael Hadley</p>
+            <p>2023</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/pages/Main.js
@@ -0,0 +1,18 @@
+const Main = () => {
+    return(
+        <div className="body">
+        <div className="intro">
+            <h1>Hi! I'm Michael. <span role="img" aria-label="emoji">👋🏻</span></h1>
+            <h2>I'm a software developer in Atlanta, GA</h2>
+            <p>I like writing code, and solving problems</p>
+            <p>I've spent time around Python, Java, React, and Node</p>
+        </div>
+        <div className="body">
+            <p>I enjoy photography, books, going to Braves baseball games <span role="img" aria-label="emoji">⚾️</span>, and cooking.</p>
+            <p>And I've got a cat named Luna<span role="img" aria-label="emoji">🐈</span></p>
+        </div>
+        </div>
+    );
+};
+
+export default Main;
